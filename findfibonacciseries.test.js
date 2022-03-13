03523dcbe6e93e94b147d32aa5a1e944a1bc71cf
Amenawon <amenawon@flutterwavego.com>
2022-03-14 00:01:27 +0100

    add test case if input of 2 was supplied

--- findfibonacciseries.test.js
+++ findfibonacciseries.test.js
@@ -8,3 +8,7 @@ test("function should return [0,1] if 1 was supplied", () => {
     const expectedResult=[0,1]
     expect(findFib(1)).toEqual(expectedResult);
 })
+test("function should return [0,1,1] if 2 was supplied", () => {
+    const expectedResult=[0,1,1]
+    expect(findFib(2)).toEqual(expectedResult);
+})
