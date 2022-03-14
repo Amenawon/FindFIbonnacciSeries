462f7b66e83eedac2ac157464c49e6376c4cb614
Amenawon <amenawon@flutterwavego.com>
2022-03-14 01:34:14 +0100

    add more describe test suites to expand test cases

--- findfibonacciseries.js
+++ findfibonacciseries.js
@@ -1,10 +1,13 @@
 
 function findFibonnacciSeries(input) {
     if (!Number.isInteger(input))
-        throw new Error('Input must be an integer');
+        return -1 //test if the input is a whole number
 
-    if (input <= 0)
-        throw new Error('Input must be a positive number');
+    if (input < 0)
+        return -1 //test if input is a positive whole number
+
+    if (input === 0)
+        return 0 //test if input is equal to 0
 
     let firstTerm = 0;
     let secondTerm = 1;
@@ -29,6 +32,6 @@ function findFibonnacciSeries(input) {
     return result;
 }
 
-findFibonnacciSeries(2)
+findFibonnacciSeries(9.2)
 
 module.exports = findFibonnacciSeries
--- findfibonacciseries.test.js
+++ findfibonacciseries.test.js
@@ -1,20 +1,59 @@
 const { expect } = require('@jest/globals');
 const findFib = require('./findfibonacciseries');
+describe('Test valid input values',()=>{
 
-test("function only accepts positive integers", () => {
-    expect(findFib(0)).toEqual(0);
+    test("function should return [0,1] if 1 was supplied", () => {
+        const expectedResult=[0,1]
+        expect(findFib(1)).toEqual(expectedResult);
+    })
+    test("function should return [0,1,1] if 2 was supplied", () => {
+        const expectedResult=[0,1,1]
+        expect(findFib(2)).toEqual(expectedResult);
+    })
+    test("function should return [0,1,1,2] if 3 was supplied", () => {
+        const expectedResult=[0,1,1,2]
+        expect(findFib(3)).toEqual(expectedResult);
+    })
+    test("function should return [0,1,1,2,3] if 4 was supplied", () => {
+        const expectedResult=[0,1,1,2,3]
+        expect(findFib(4)).toEqual(expectedResult);
+    })
+    test("function should return [0,1,1,2,3,5,8,13,21,34,55] if 10 was supplied", () => {
+        const expectedResult=[0,1,1,2,3,5,8,13,21,34,55]
+        expect(findFib(10)).toEqual(expectedResult);
+    })
 })
-test("function should return [0,1] if 1 was supplied", () => {
-    const expectedResult=[0,1]
-    expect(findFib(1)).toEqual(expectedResult);
-})
-test("function should return [0,1,1] if 2 was supplied", () => {
-    const expectedResult=[0,1,1]
-    expect(findFib(2)).toEqual(expectedResult);
-})
-test("function only accepts whole numbers", () => {
-    expect(findFib(7.3)).toEqual(0);
+
+describe('test the data type of the Parameter of the Fibonacci method', () => {
+ 
+    it('should return -1 if a string is passed', () => {
+        expect(findFib('2')).toEqual(-1);
+    });
+    it('should return -1 if an array is passed', () => {
+        expect(findFib(['2'])).toEqual(-1);
+    });
+    it('should return -1 if a floating number is passed', () => {
+        expect(findFib(1.100)).toEqual(-1);
+    });
+    it('should NOT should return -1 if a positive whole number is passed', () => {
+        expect(findFib(2)).not.toEqual(-1);
+    });
 });
-test("function should return 0 if 0 is supplied", () => {
-    expect(findFib(0)).toEqual(0);
-})
+describe('test if value of the input is a positive number', () => {
+
+    it('should return -1 if -1 is passed', () => {
+        expect(findFib(-1)).toEqual(-1);
+    });
+    it('should return -1 if -100 is passed', () => {
+        expect(findFib(-100)).toEqual(-1);
+    });
+    it('should not return -1 if 0 is passed', () => {
+        expect(findFib(0)).not.toEqual(-1);
+    });
+    it('should not return -1 if  1 is passed', () => {
+        expect(findFib(1)).not.toEqual(-1);
+    });
+    it('should not throw the exception: "The Parameter must be positive!"', () => {
+        expect(findFib(2)).not.toEqual(-1);
+    });
+});
